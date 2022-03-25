2c8eff8f2afa96314bfd362a06b743c189e55123
FiggCoder <figgcoder@gmail.com>
2022-03-25 15:45:04 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-sudo yum -y groupinstall "Development Tools"
+# sudo yum -y groupinstall "Development Tools"
 
-sudo yum -y install libuuid-devel util-linux libtool gperf openssl-devel openssl-libs
+# sudo yum -y install libuuid-devel util-linux libtool gperf openssl-devel openssl-libs
 
-mkdir -p ~/tmp/{usr,etc,var,libs,install,downloads,tar}
+# mkdir -p ~/tmp/{usr,etc,var,libs,install,downloads,tar}
 
-wget -P ~/tmp/downloads \
-         https://downloads.sourceforge.net/freetype/freetype-2.11.1.tar.xz \
-         https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.1.tar.bz2 \
-         https://download.gnome.org/sources/libxml2/2.9/libxml2-2.9.13.tar.xz \
-         https://poppler.freedesktop.org/poppler-21.06.1.tar.xz \
-         https://poppler.freedesktop.org/poppler-data-0.4.11.tar.gz \
-&& ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
+# wget -P ~/tmp/downloads \
+#          https://downloads.sourceforge.net/freetype/freetype-2.11.1.tar.xz \
+#          https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.1.tar.bz2 \
+#          https://download.gnome.org/sources/libxml2/2.9/libxml2-2.9.13.tar.xz \
+#          https://poppler.freedesktop.org/poppler-21.06.1.tar.xz \
+#          https://poppler.freedesktop.org/poppler-data-0.4.11.tar.gz \
+# && ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
 
 # pushd .
 
 # ####################################
-# cd ~/tmp/libs/freetype*
-# sed -e "/AUX.*.gxvalid/s@^# @@" \
-#     -e "/AUX.*.otvalid/s@^# @@" \
-#     -i modules.cfg              &&
+cd ~/tmp/libs/freetype*
+sed -e "/AUX.*.gxvalid/s@^# @@" \
+    -e "/AUX.*.otvalid/s@^# @@" \
+    -i modules.cfg              &&
 
-# # sed -e 's:.*\(#.*SUBPIXEL.*\) .*:\1:' \
-# #     -i include/freetype/config/ftoption.h  &&
+# sed -e 's:.*\(#.*SUBPIXEL.*\) .*:\1:' \
+#     -i include/freetype/config/ftoption.h  &&
 
-# ./configure --prefix=/home/ec2-user/tmp/usr --disable-static
-#  &&
-# make
-# make install 
+./configure --prefix=/home/ec2-user/tmp/usr --disable-static
+ &&
+make
+make install 
 
 # ####################################
 # cd ~/tmp/libs/libxml*
