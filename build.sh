bf8deb7e2f82720dd05d421fbdf3eb7946fb04d1
FiggCoder <figgcoder@gmail.com>
2022-03-25 23:25:48 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,80 +16,80 @@ wget -P ~/tmp/downloads \
          https://poppler.freedesktop.org/poppler-data-0.4.8.tar.gz \
 && ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
 
-pushd .
+# pushd .
 
-####################################
-#cd ~/tmp/libs/cmake*
-#./configure
-#make
-#sudo make install
+# ####################################
+# #cd ~/tmp/libs/cmake*
+# #./configure
+# #make
+# #sudo make install
 
-####################################
+# ####################################
 
 
-####################################
-cd ~/tmp/libs/freetype*
-sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg &&
+# ####################################
+# cd ~/tmp/libs/freetype*
+# sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg &&
 
-sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
-    -i include/freetype/config/ftoption.h  &&
+# sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
+#     -i include/freetype/config/ftoption.h  &&
 
-./configure --prefix=/home/ec2-user/tmp/usr --disable-static
-make
-make install 
+# ./configure --prefix=/home/ec2-user/tmp/usr --disable-static
+# make
+# make install 
 
-####################################
-cd ~/tmp/libs/libxml*
-PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
-./configure --prefix=/home/ec2-user/tmp/usr --disable-static --with-history --without-python
-make
-make install
+# ####################################
+# cd ~/tmp/libs/libxml*
+# PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
+# ./configure --prefix=/home/ec2-user/tmp/usr --disable-static --with-history --without-python
+# make
+# make install
 
-####################################
-cd ~/tmp/libs/fontconfig*
-export FONTCONFIG_PKG=`pwd`
+# ####################################
+# cd ~/tmp/libs/fontconfig*
+# export FONTCONFIG_PKG=`pwd`
 
-PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
-./configure --prefix=/home/ec2-user/tmp/usr        \
-            --sysconfdir=/home/ec2-user/tmp/etc    \
-            --localstatedir=/home/ec2-user/tmp/var \
-            --disable-docs       \
-            --enable-libxml2 
-make
-make install
+# PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
+# ./configure --prefix=/home/ec2-user/tmp/usr        \
+#             --sysconfdir=/home/ec2-user/tmp/etc    \
+#             --localstatedir=/home/ec2-user/tmp/var \
+#             --disable-docs       \
+#             --enable-libxml2 
+# make
+# make install
 
-####################################
-cd ~/tmp/libs/poppler-0*
-#PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
-#./configure --prefix=/var/task      \
-#            --sysconfdir=/var/task/etc           \
-#            --enable-build-type=release \
-#            --enable-cmyk               \
-#            --enable-xpdf-headers && make
-mkdir -p build
-cd build
-PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
-cmake .. -DCMAKE_BUILD_TYPE=Release   \
-       -DCMAKE_INSTALL_PREFIX=/home/ec2-user/tmp/usr  \
-       -DSYSCONFDIR=/var/task/etc \
-       -DSPLASH_CMYK=ON \
-       -DTESTDATADIR=$PWD/testfiles \
-       -DENABLE_XPDF_HEADERS=ON     \
-       -DENABLE_LIBOPENJPEG=none  \
-       -DENABLE_CMS=none  \
-       -DENABLE_DCTDECODER=none \
-&& make &&
-make install DESTDIR="/home/ec2-user/tmp/install"
+# ####################################
+# cd ~/tmp/libs/poppler-0*
+# #PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
+# #./configure --prefix=/var/task      \
+# #            --sysconfdir=/var/task/etc           \
+# #            --enable-build-type=release \
+# #            --enable-cmyk               \
+# #            --enable-xpdf-headers && make
+# mkdir -p build
+# cd build
+# PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
+# cmake .. -DCMAKE_BUILD_TYPE=Release   \
+#        -DCMAKE_INSTALL_PREFIX=/home/ec2-user/tmp/usr  \
+#        -DSYSCONFDIR=/var/task/etc \
+#        -DSPLASH_CMYK=ON \
+#        -DTESTDATADIR=$PWD/testfiles \
+#        -DENABLE_XPDF_HEADERS=ON     \
+#        -DENABLE_LIBOPENJPEG=none  \
+#        -DENABLE_CMS=none  \
+#        -DENABLE_DCTDECODER=none \
+# && make &&
+# make install DESTDIR="/home/ec2-user/tmp/install"
 
-cd ~/tmp/libs/poppler-data*
-make prefix=/home/ec2-user/tmp/usr install
+# cd ~/tmp/libs/poppler-data*
+# make prefix=/home/ec2-user/tmp/usr install
 
-unset FONTCONFIG_PKG
-popd
+# unset FONTCONFIG_PKG
+# popd
 
-tar -C ~/tmp/install/var/task \
-    --exclude='include' \
-    --exclude='share'   \
-    -zcvf ~/tmp/tar/poppler.tar.gz .
+# tar -C ~/tmp/install/var/task \
+#     --exclude='include' \
+#     --exclude='share'   \
+#     -zcvf ~/tmp/tar/poppler.tar.gz .
 
-# aws s3 cp ~/tmp/tar/poppler.tar.gz s3://"${S3BUCKET}"/poppler.tar.gz
+# # aws s3 cp ~/tmp/tar/poppler.tar.gz s3://"${S3BUCKET}"/poppler.tar.gz
