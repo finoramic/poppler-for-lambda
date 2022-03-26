61f904e64c8817bf70e62fb5e22fdc1f7c5d8f8b
FiggCoder <figgcoder@gmail.com>
2022-03-25 23:41:36 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,22 +28,22 @@ set -e
 
 
 # ####################################
-cd ~/tmp/libs/freetype*
-sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg &&
+# cd ~/tmp/libs/freetype*
+# sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg &&
 
-sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
-    -i include/freetype/config/ftoption.h  &&
+# sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
+#     -i include/freetype/config/ftoption.h  &&
 
-./configure --prefix=/home/ec2-user/tmp/usr --disable-static
-make
-make install 
+# ./configure --prefix=/home/ec2-user/tmp/usr --disable-static
+# make
+# make install 
 
 # ####################################
-# cd ~/tmp/libs/libxml*
-# PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
-# ./configure --prefix=/home/ec2-user/tmp/usr --disable-static --with-history --without-python
-# make
-# make install
+cd ~/tmp/libs/libxml*
+PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
+./configure --prefix=/home/ec2-user/tmp/usr --disable-static --with-history --without-python
+make
+make install
 
 # ####################################
 # cd ~/tmp/libs/fontconfig*
