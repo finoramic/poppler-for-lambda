b86056bea22313d8132790561c4819442847b4b0
FiggCoder <figgcoder@gmail.com>
2022-03-25 23:31:47 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-sudo yum -y groupinstall "Development Tools"
-sudo yum -y install gperf zlib-devel
+# sudo yum -y groupinstall "Development Tools"
+# sudo yum -y install gperf zlib-devel
 
-mkdir -p ~/tmp/{usr,etc,var,libs,install,downloads,tar}
+# mkdir -p ~/tmp/{usr,etc,var,libs,install,downloads,tar}
 
-wget -P ~/tmp/downloads \
-         http://downloads.sourceforge.net/freetype/freetype-2.9.tar.bz2 \
-         http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.6.tar.bz2 \
-         https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz \
-         http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz \
-         https://poppler.freedesktop.org/poppler-0.62.0.tar.xz \
-         https://poppler.freedesktop.org/poppler-data-0.4.8.tar.gz \
-&& ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
+# wget -P ~/tmp/downloads \
+#          http://downloads.sourceforge.net/freetype/freetype-2.9.tar.bz2 \
+#          http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.6.tar.bz2 \
+#          https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz \
+#          http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz \
+#          https://poppler.freedesktop.org/poppler-0.62.0.tar.xz \
+#          https://poppler.freedesktop.org/poppler-data-0.4.8.tar.gz \
+# && ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
 
 # pushd .
 
 # ####################################
-# #cd ~/tmp/libs/cmake*
-# #./configure
-# #make
-# #sudo make install
+cd ~/tmp/libs/cmake*
+./bootstrap
+make
+sudo make install
 
 # ####################################
 
