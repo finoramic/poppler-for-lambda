87fbb51be0cb8cff72ac56b084392d0adfbf9f0c
FiggCoder <figgcoder@gmail.com>
2022-08-06 11:04:20 -0700

    starting again

--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-# sudo yum -y groupinstall "Development Tools"
-# sudo yum -y install gperf zlib-devel libjpeg-turbo-devel boost-devel
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install gperf zlib-devel libjpeg-turbo-devel boost-devel
 
-# mkdir -p ~/tmp/{usr,etc,var,libs,install,downloads,tar}
+mkdir -p ~/tmp/{usr,etc,var,libs,install,downloads,tar}
 
-# wget -P ~/tmp/downloads \
-#          http://downloads.sourceforge.net/freetype/freetype-2.9.tar.bz2 \
-#          http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.6.tar.bz2 \
-#          https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz \
-#          http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz \
-#          https://poppler.freedesktop.org/poppler-21.06.0.tar.xz \
-#          https://poppler.freedesktop.org/poppler-data-0.4.8.tar.gz \
-# && ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
+wget -P ~/tmp/downloads \
+          http://downloads.sourceforge.net/freetype/freetype-2.9.tar.bz2 \
+          http://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.6.tar.bz2 \
+          https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz \
+          http://xmlsoft.org/sources/libxml2-2.9.7.tar.gz \
+          https://poppler.freedesktop.org/poppler-21.06.0.tar.xz \
+          https://poppler.freedesktop.org/poppler-data-0.4.8.tar.gz \
+       && ls ~/tmp/downloads/*.tar.* | xargs -i tar xf {} -C ~/tmp/libs/
 
-# pushd .
+pushd .
 
 # ####################################
 # cd ~/tmp/libs/cmake*
@@ -46,8 +46,8 @@ set -e
 # make install
 
 # ####################################
-cd ~/tmp/libs/fontconfig*
-export FONTCONFIG_PKG=`pwd`
+# cd ~/tmp/libs/fontconfig*
+# export FONTCONFIG_PKG=`pwd`
 
 # PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$PKG_CONFIG_PATH \
 # ./configure --prefix=/home/ec2-user/tmp/usr        \
@@ -59,23 +59,23 @@ export FONTCONFIG_PKG=`pwd`
 # make install
 
 # ####################################
-cd ~/tmp/libs/poppler-21*
+# cd ~/tmp/libs/poppler-21*
 
-mkdir -p build
-cd build
-PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
-cmake .. -DCMAKE_BUILD_TYPE=Release   \
-       -DCMAKE_INSTALL_PREFIX=/home/ec2-user/tmp/usr  \
-       -DSYSCONFDIR=/var/task/etc \
-       -DSPLASH_CMYK=ON \
-       -DTESTDATADIR=$PWD/testfiles \
-       -DENABLE_XPDF_HEADERS=ON     \
-       -DENABLE_LIBOPENJPEG=none  \
-       -DENABLE_CMS=none  \
-       -DENABLE_DCTDECODER=none \
-       -DENABLE_BOOST=OFF \
-&& make &&
-make install DESTDIR="/home/ec2-user/tmp/install"
+# mkdir -p build
+# cd build
+# PKG_CONFIG_PATH=~/tmp/usr/lib/pkgconfig/:$FONTCONFIG_PKG:$PKG_CONFIG_PATH \
+# cmake .. -DCMAKE_BUILD_TYPE=Release   \
+#        -DCMAKE_INSTALL_PREFIX=/home/ec2-user/tmp/usr  \
+#        -DSYSCONFDIR=/var/task/etc \
+#        -DSPLASH_CMYK=ON \
+#        -DTESTDATADIR=$PWD/testfiles \
+#        -DENABLE_XPDF_HEADERS=ON     \
+#        -DENABLE_LIBOPENJPEG=none  \
+#        -DENABLE_CMS=none  \
+#        -DENABLE_DCTDECODER=none \
+#        -DENABLE_BOOST=OFF \
+# && make &&
+# make install DESTDIR="/home/ec2-user/tmp/install"
 
 # cd ~/tmp/libs/poppler-data*
 # make prefix=/home/ec2-user/tmp/usr install
